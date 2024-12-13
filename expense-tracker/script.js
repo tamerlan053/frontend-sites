853985adce4fc2b72eb1ffd551c966bc26446dd2
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-13 10:07:37 +0100

    addTransaction() function

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,6 +1,33 @@
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
 const localStorageTransactions = JSON.parse(
   localStorage.getItem('transactions')
 );
 
-let transactions =
-  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
