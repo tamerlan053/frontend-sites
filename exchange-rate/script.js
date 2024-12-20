b1948b2e43a4206b22a76f1fe547bdbcc5c98698
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-20 21:13:14 +0100

    elements selected in js

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,1 +1,6 @@
-
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
