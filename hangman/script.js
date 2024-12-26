ff7c4ce9c4c68547758a843ca6ed5d30eefbf960
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-26 22:30:49 +0100

    Display parts in updateWrongLettersEl()

--- hangman/script.js
+++ hangman/script.js
@@ -48,4 +48,14 @@ function updateWrongLettersEl() {
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map(letter => `<span>${letter}</span>`)}
   `;
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
 }
